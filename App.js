157f383f5445365469317e79d429a8c7c6fc4629
André <andreluisflor@fittelecom.com.br>
2019-07-31 17:21:27 -0300

    initial

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, MainText } from "./styles";
+
+const App = () => {
+  return (
+    <Container>
+      <MainText>Para começar vamos precisar do seu RG. Ok?</MainText>
+    </Container>
+  );
+};
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+  display: flex;
+  flex: 1;
+  align-items: flex-start;
+  padding: 15px;
+`;
+
+export const MainText = styled.Text`
+  font-family: monospace;
+  font-size: 20px;
+  font-weight: bold;
+`;
